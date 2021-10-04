f0d418937f4fd5ddbad19fdf73e2b1df8697a576
RayanlFerreira <rayanlyung2@gmail.com>
2021-10-04 19:42:13 -0300

    Primeiro código.
    Código feito para converter celsius em fahrenheit.

--- Código C#/Program.cs
+++ Código C#/Program.cs
@@ -7,6 +7,17 @@ namespace Código_C_
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            /*1-Ler temperatura em graus Celsius e transformá-la em graus Fahrenheit*/
+            int celsius, fahrenheit;
+
+            Console.WriteLine("Me diga a temperatura em graus celsius:");
+            celsius = int.Parse(Console.ReadLine());
+
+            fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine("A temperatura em fahrenheit é " + fahrenheit +"°F");
+            /*----------------------------------------------------------------------*/
         }
     }
 }
