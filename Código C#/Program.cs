768d67fee7ba730dbaba8427f78c0903720e781b
RayanlFerreira <rayanlyung2@gmail.com>
2021-10-04 19:22:33 -0300

    Criei o  console C#
    Criei o console do c# pelo terminal.

---
+++ Código C#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Código_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
