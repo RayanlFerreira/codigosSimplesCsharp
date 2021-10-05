0424c0f04ff9d331414b95509e6b65ce166eb470
RayanlFerreira <rayanlyung2@gmail.com>
2021-10-05 14:34:44 -0300

    2 código feito
    Cálculo de um saláriolíquido de um professor. Serão fornecidos valor dahora aula, númerode aulas dadas e o % de desconto do INSS.

--- Código C#/Program.cs
+++ Código C#/Program.cs
@@ -6,18 +6,37 @@ namespace Código_C_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá Mundo");
 
             /*1-Ler temperatura em graus Celsius e transformá-la em graus Fahrenheit*/
-            int celsius, fahrenheit;
+            /*
+            float celsius, fahrenheit;
 
             Console.WriteLine("Me diga a temperatura em graus celsius:");
             celsius = int.Parse(Console.ReadLine());
 
             fahrenheit = (celsius * 9 / 5) + 32;
-
             Console.WriteLine("A temperatura em fahrenheit é " + fahrenheit +"°F");
+            */
             /*----------------------------------------------------------------------*/
+
+            /*2-Cálculo de um saláriolíquido de um professor. Serão fornecidos valor dahora aula, númerode aulas dadas e o % de desconto do INSS.*/
+            float dinheiroHora,INSS,salario;
+            int aulas;
+
+            Console.WriteLine("Diga o valor que você recebe por horas de aulas: ");
+            dinheiroHora = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Quantas aulas foram dadas ? ");
+            aulas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a porcentagem de desconto do INSS de acordo com seu salário? ");
+            INSS = float.Parse(Console.ReadLine());
+
+            salario = (dinheiroHora * aulas);
+            salario = salario - (salario * INSS/100);
+
+            Console.WriteLine("Seu salário é de " + salario +" reais.");
         }
     }
 }
