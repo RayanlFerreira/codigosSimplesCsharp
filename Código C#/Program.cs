0e52da40258bd4d1c3bd62daa2e4b657425471b9
RayanlFerreira <rayanlyung2@gmail.com>
2021-10-05 15:45:36 -0300

    Código 4,5 e 6.
    4- Faça um programa que calcule e mostre a área de um círculo.: Obs.: Crie uma constante para π, sabendo que ele vale 3.1415.
    
    5- Faça  um  programa  que  receba  o  ano  de  nascimento  de  uma pessoa  e  o  ano  atual,  calcule  e  mostre:  a.  A  idade  dessa  pessoa;  b.  Quantos anos essa pessoa terá em 2022.
    
    6- O custo ao consumidor de um carro novo é a soma do preço de fábrica  com  o  percentual  de  lucro  do  distribuidor  e  dos  impostos  aplicados  ao preço  de  fábrica.  Faça  um  programa  que  receba  o  preço  de  fábrica  de  um veículo, o percentual de lucro do distribuidor e o percentual de imposto. Calcule e  mostre:  a.  O  valor  correspondente  ao  lucro  do  distribuidor;  b.  O  valor correspondente aos impostos; c. O preço final do veículo.

--- Código C#/Program.cs
+++ Código C#/Program.cs
@@ -58,6 +58,57 @@ namespace Código_C_
             }
             */
             /*----------------------------------------------------------------------*/
+
+            /*4- Faça um programa que calcule e mostre a área de um círculo.: Obs.: Crie uma constante para π, sabendo que ele vale 3.1415.*/
+            /*
+            float raio;
+            const double pi = 3.1415;
+            double area;
+
+            Console.WriteLine("Me diga o valor do raio de um circulo : ");
+            raio = float.Parse(Console.ReadLine());
+
+            area = pi * (raio * raio);
+
+            Console.WriteLine("A área da circunferência é: " + area);
+            */
+            /*----------------------------------------------------------------------*/
+
+            /*5- Faça  um  programa  que  receba  o  ano  de  nascimento  de  uma pessoa  e  o  ano  atual,  calcule  e  mostre:  a.  A  idade  dessa  pessoa;  b.  Quantos anos essa pessoa terá em 2022.*/
+            /*
+            int anonascimento, idade, idade2022;
+            const int ano2022 = 2022;
+            int anoAtual = 2021;
+
+            Console.WriteLine("Me diga o ano em que você nasceu :");
+            anonascimento = int.Parse(Console.ReadLine());
+
+            idade = anoAtual - anonascimento;
+            idade2022 = ano2022-anonascimento;
+            Console.WriteLine("Você atualmente tem "+ idade +"anos e em 2022 você terá "+ idade2022);
+            */
+            /*----------------------------------------------------------------------*/
+
+            /*6- O custo ao consumidor de um carro novo é a soma do preço de fábrica  com  o  percentual  de  lucro  do  distribuidor  e  dos  impostos  aplicados  ao preço  de  fábrica.  Faça  um  programa  que  receba  o  preço  de  fábrica  de  um veículo, o percentual de lucro do distribuidor e o percentual de imposto. Calcule e  mostre:  a.  O  valor  correspondente  ao  lucro  do  distribuidor;  b.  O  valor correspondente aos impostos; c. O preço final do veículo.*/
+            float valorF, imposto, lucro, preco;
+
+            Console.WriteLine("Digite o valor do veículo vindo da fábrica: ");
+            valorF = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o imposto aplicado no valor do veículo vindo da fábrica: ");
+            imposto = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o percentual de lucro? ");
+            lucro = float.Parse(Console.ReadLine());
+
+            imposto = valorF * (imposto/100);
+            lucro = valorF * (lucro/100);
+
+            preco = valorF + imposto + lucro;
+
+            Console.WriteLine("O valor de lucro do distribuidor é R$" + lucro);
+            Console.WriteLine("O valor do imposto é R$" + imposto);
+            Console.WriteLine("O preço do veículo é R$" + preco);
         }
     }
 }
